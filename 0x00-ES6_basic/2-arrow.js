49708ae30dfc4a01afc1dfbfd08b9815325d0aaf
yeniesew <yebrielij@gmail.com>
2023-09-25 11:54:41 +0000

    2-4

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default class getNeighborhoodsList {
+  constructor() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+    this.addNeighborhood = (newNeighborhood) => {
+      this.sanFranciscoNeighborhoods.push(newNeighborhood);
+      return this.sanFranciscoNeighborhoods;
+    };
+  }
+}
